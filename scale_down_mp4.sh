6ea7be15e06f800275e85886cbd7e50818adc26f
amandaghassaei <amandaghassaei@gmail.com>
2021-03-26 10:57:48 -0700

    scale down mp4

---
+++ scale_down_mp4.sh
@@ -0,0 +1,17 @@
+# Save small (no audio) video file to mp4.
+
+# Pipe output to txt file.
+exec 2>&1
+
+# Enter the first file's directory.
+current_path=$(dirname "$1")
+cd "$current_path"
+
+# For every selected file, save mp4.
+for f in "$@"
+do
+	filenameNoExt=${f%.*}
+	/usr/local/bin/ffmpeg -i "$f" -vf scale=800:-1 -c:v libx264 -preset slow -crf 22 -pix_fmt yuv420p -an -y "$filenameNoExt-small.mp4"
+done
+
+exit 0
